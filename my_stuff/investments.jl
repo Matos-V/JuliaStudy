6efd4043ecdfbb9457c7ed13551a2d8765d4a0d0
Vitor Matos <vitor.matos@ee.ufcg.edu.br>
2022-06-25 22:08:29 -0300

    code refactor

--- my_stuff/investments.jl
+++ my_stuff/investments.jl
@@ -1,35 +1,63 @@
-valor_da_cota = 7.38
-DY_anual = false
-if DY_anual
-    DY = 6/100
-    DY_mensal = (1+DY)^(1/12) - 1
-else
-    DY_mensal = 0.87/100
+mutable struct Investment
+    stock_name::String
+    stock_price::Float64
+    DY_year::Float64
+    monthly_contribution::Float64
+    expected_earning::Float64
+    reinvest_dividends::Bool
+
+    dividend::Float64
+    months::Int64
+    patrimony::Float64
+    DY_month::Float64
+    n_expected_stocks::Int64
+    n_stocks::Int64
+    rest::Float64
+    isActive
+
+    function Investment(stock_name::String,
+                        stock_price::Float64,
+                        DY_year::Float64,
+                        monthly_contribution::Float64,
+                        expected_earning::Float64,
+                        reinvest_dividends::Bool,)
+        DY_month = (1+DY_year/100)^(1/12) - 1
+        dividend = stock_price*DY_month
+        n_expected_stocks = expected_earning ÷ dividend
+
+        new(stock_name,
+            stock_price,
+            DY_year,
+            monthly_contribution,
+            expected_earning,
+            reinvest_dividends,
+            dividend,
+            0,
+            0,
+            DY_month,
+            n_expected_stocks,
+            0,
+            0,
+            true)
+    end
 end
-rendimento_esperado = 2000
-aporte_mensal = 600
 
-rendimento = valor_da_cota*DY_mensal
-n_cotas_esperado = rendimento_esperado / rendimento
-meses = 0
-patrimonio = 0
-n_cotas = 0
-resto = 0
-reinvestir_dividendos = true
+BBAS3 = Investment("BBAS3", 34.0, 10.0, 500.0, 500.0, true)
 
-while n_cotas <= n_cotas_esperado
-    global meses = meses + 1
-    if reinvestir_dividendos
-        global n_cotas = n_cotas + (aporte_mensal + n_cotas*rendimento + resto)÷valor_da_cota
-        global resto = (aporte_mensal + n_cotas*rendimento + resto) % valor_da_cota
+while BBAS3.n_stocks <= BBAS3.n_expected_stocks
+    BBAS3.months = BBAS3.months + 1
+    if BBAS3.reinvest_dividends
+        BBAS3.n_stocks = BBAS3.n_stocks + (BBAS3.monthly_contribution + BBAS3.n_stocks*BBAS3.dividend + BBAS3.rest)÷BBAS3.stock_price
+        BBAS3.rest = (BBAS3.monthly_contribution + BBAS3.n_stocks*BBAS3.dividend + BBAS3.rest) % BBAS3.stock_price
     else
-        global n_cotas = n_cotas + (aporte_mensal + resto)÷valor_da_cota
-        global resto = (aporte_mensal + resto) % valor_da_cota
+        BBAS3.n_stocks = BBAS3.n_stocks + (BBAS3.monthly_contribution + BBAS3.rest)÷BBAS3.stock_price
+        BBAS3.rest = (BBAS3.monthly_contribution + BBAS3.rest) % BBAS3.stock_price
         
     end
-    #global valor_da_cota = round(valor_da_cota * (1+ rand(-3:0.01:3)/100), digits=2)
+    #global stock_price = round(stock_price * (1+ rand(-3:0.01:3)/100), digits=2)
 end
-patrimonio = n_cotas*valor_da_cota + resto
+BBAS3.patrimony = BBAS3.n_stocks*BBAS3.stock_price + BBAS3.rest
     
-println("Você acumulou $n_cotas cotas em $meses meses, acumulando um patrimonio de $(round(patrimonio,digits=2))!")
-println("O valor da cota ao final do percurso é de $valor_da_cota:")
+println("You have acumulated $(BBAS3.n_stocks) quotas in $(BBAS3.months) months with total patrimony of \$$(round(BBAS3.patrimony,digits=2))!")
+println("O valor da cota ao final do percurso é de $(BBAS3.stock_price):")
+println(BBAS3)
