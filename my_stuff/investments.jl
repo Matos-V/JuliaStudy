39a3b04043041a55bb475c130345503e4497f412
Vitor Matos <vitor.matos@ee.ufcg.edu.br>
2022-10-12 22:58:49 -0300

    minor improvements

--- my_stuff/investments.jl
+++ my_stuff/investments.jl
@@ -1,10 +1,9 @@
-mutable struct Investment
+mutable struct Stock
     stock_name::String
     stock_price::Float64
     DY_year::Float64
     monthly_contribution::Float64
     expected_earning::Float64
-    reinvest_dividends::Bool
 
     dividend::Float64
     months::Int64
@@ -15,12 +14,12 @@ mutable struct Investment
     rest::Float64
     isActive
 
-    function Investment(stock_name::String,
+    function Stock(stock_name::String,
                         stock_price::Float64,
                         DY_year::Float64,
                         monthly_contribution::Float64,
-                        expected_earning::Float64,
-                        reinvest_dividends::Bool,)
+                        expected_earning::Float64,)
+
         DY_month = (1+DY_year/100)^(1/12) - 1
         dividend = stock_price*DY_month
         n_expected_stocks = expected_earning ÷ dividend
@@ -30,7 +29,6 @@ mutable struct Investment
             DY_year,
             monthly_contribution,
             expected_earning,
-            reinvest_dividends,
             dividend,
             0,
             0,
@@ -42,7 +40,7 @@ mutable struct Investment
     end
 end
 
-function invest_with_dividends(stock::Investment)
+function invest_with_dividends(stock::Stock)
     while stock.n_stocks <= stock.n_expected_stocks
         stock.months = stock.months + 1
         stock.n_stocks = stock.n_stocks + (stock.monthly_contribution + stock.n_stocks*stock.dividend + stock.rest)÷stock.stock_price
@@ -51,7 +49,7 @@ function invest_with_dividends(stock::Investment)
     stock.patrimony = stock.n_stocks*stock.stock_price + stock.rest
 end
 
-function invest_without_dividends(stock::Investment)
+function invest_without_dividends(stock::Stock)
     while stock.n_stocks <= stock.n_expected_stocks
         stock.months = stock.months + 1
         stock.n_stocks = stock.n_stocks + (stock.monthly_contribution + stock.rest)÷stock.stock_price
@@ -60,21 +58,25 @@ function invest_without_dividends(stock::Investment)
     stock.patrimony = stock.n_stocks*stock.stock_price + stock.rest
 end
 
-BBAS3 = Investment("Banco do Brasil", #  stock_name::String
-                    34.0,   #  stock_price::Float64
-                    10.0,   #  DY_year::Float64
-                    500.0,  #  monthly_contribution::Float64
-                    1000.0,  #  expected_earning::Float64
-                    true)  #  reinvest_dividends::Bool
+BBAS3_with_dividends = Stock("Banco do Brasil", #  stock_name::String
+                            35.0,   #  stock_price::Float64
+                            10.0,   #  DY_year::Float64
+                            550.0,  #  monthly_contribution::Float64
+                            5000.0,)  #  expected_earning::Float64
+
+BBAS3_without_dividends = Stock("Banco do Brasil", #  stock_name::String
+                                35.0,   #  stock_price::Float64
+                                10.0,   #  DY_year::Float64
+                                550.0,  #  monthly_contribution::Float64
+                                5000.0,)  #  expected_earning::Float64
 
 
 # stock_price = round(stock_price * (1+ rand(-3:0.01:3)/100), digits=2)
-if BBAS3.reinvest_dividends
-    invest_with_dividends(BBAS3)
-else
-    invest_without_dividends(BBAS3)
-end
+invest_with_dividends(BBAS3_with_dividends)
+
+invest_without_dividends(BBAS3_without_dividends)
 
 
-
-println("You have acumulated $(BBAS3.n_stocks) quotas in $(BBAS3.months) months with total patrimony of \$$(round(BBAS3.patrimony,digits=2))!")
+println("Reinvesting dividends: You have acumulated $(BBAS3_with_dividends.n_stocks) quotas in $(round(BBAS3_with_dividends.months/12,digits=2)) years with total patrimony of \$$(round(BBAS3_with_dividends.patrimony,digits=2))!")
+println()
+println("Without reinvesting dividends: You have acumulated $(BBAS3_without_dividends.n_stocks) quotas in $(round(BBAS3_without_dividends.months/12,digits=2)) years with total patrimony of \$$(round(BBAS3_without_dividends.patrimony,digits=2))!")
